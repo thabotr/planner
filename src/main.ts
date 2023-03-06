224ccd57e5922e5d1b084c366b31c8ad5641177f
Thabo Ranamane <thabo@synthesis.co.za>
2023-03-06 10:30:21 +0200

    added vuetify and material design icons

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,28 @@ import './assets/main.css'
 
 const app = createApp(App)
 
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import '@mdi/font/css/materialdesignicons.css'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+        mdi,
+    }
+  }
+})
+
 app.use(createPinia())
 app.use(router)
+app.use(vuetify)
 
 app.mount('#app')
