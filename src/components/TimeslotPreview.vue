cbf560b25dd624c60be57c01ef488f8e178795a1
Thabo Ranamane <thabo@synthesis.co.za>
2023-03-16 16:15:29 +0200

    added timeslot preview card

---
+++ src/components/TimeslotPreview.vue
@@ -0,0 +1,68 @@
+<template>
+    <div>
+        <v-card>
+            <div class="flex-horizontal">
+                <EffortPreviewVue :item="timeslot">
+                    <template #before-mep>{{ consumedMEP }}/</template>
+                    <template #before-pep>{{ consumedPEP }}/</template>
+                    <template #before-time>{{ consumedTime }}/</template>
+                </EffortPreviewVue>
+                <div id="actions" class="flex-vertical justify-space-between">
+                    <v-chip density="compact">
+                        {{ timeslot.id }}
+                    </v-chip>
+                    <v-btn icon="mdi-pencil" density="compact" flat aria-label="edit" @click="$emit('edit')"></v-btn>
+                </div>
+            </div>
+        </v-card>
+    </div>
+</template>
+
+<script lang="ts">
+import { toSubjectiveEffortScore, verboseTimestamp } from '@/middleware/helpers';
+import ItemType from '@/types/ItemType';
+import EffortPreviewVue from './Task/EffortPreview.vue';
+export default {
+    props: {
+        timeslot: {
+            type: ItemType,
+            required: true,
+        },
+        usage: {
+            type: ItemType,
+            required: true,
+        },
+    },
+    computed: {
+        consumedMEP() { return toSubjectiveEffortScore(this.usage.mES); },
+        consumedPEP() { return toSubjectiveEffortScore(this.usage.pES); },
+        consumedTime() { return verboseTimestamp(this.usage.length); },
+    }
+    ,
+    components: { EffortPreviewVue, }
+}
+
+</script>
+
+<style style>
+.v-btn {
+    background-color: transparent;
+}
+
+#actions .v-chip {
+    background-color: var(--color-secondary);
+}
+
+#actions {
+    padding: 0.2rem;
+}
+
+.v-btn {
+    background-color: transparent;
+    color: var(--color-primary);
+}
+
+.v-card {
+    background-image: linear-gradient(var(--color-accent), var(--color-highlight));
+}
+</style>
---
+++ src/components/__tests__/TImeslotPreview.spec.ts
@@ -0,0 +1,24 @@
+import { TimeInMillis } from "@/middleware/helpers";
+import ItemType from "@/types/ItemType";
+import { describe, expect, it } from "vitest";
+import TimeslotPreviewVue from "../TimeslotPreview.vue";
+import { CustomVueWrapper } from "./utils";
+
+describe('TimeslotPreview', () => {
+    const timeslot: ItemType = new ItemType(TimeInMillis.Day, 10, 20, "id");
+    const usage: ItemType = new ItemType(TimeInMillis.Hour, 0, 10, "id");
+    const wrapper = new CustomVueWrapper(TimeslotPreviewVue, {
+        props: {
+            timeslot: timeslot,
+            usage: usage,
+        },
+    });
+    it('renders correctly', () => {
+        expect(wrapper.wrapper.html()).toMatchSnapshot();
+    });
+    it("emits 'edit' event on click edit", async () => {
+        expect(wrapper.wrapper.emitted()).not.toHaveProperty('edit');
+        wrapper.click('edit');
+        expect(wrapper.wrapper.emitted()).toHaveProperty('edit');
+    });
+});
