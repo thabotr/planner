f65a43b27c521aad070c1e27573a4cb0bb8e4108
Thabo Ranamane <thabo@synthesis.co.za>
2023-03-19 15:14:03 +0200

    fixed drag functionality

--- src/components/Task/UnscheduledTaskCard.vue
+++ src/components/Task/UnscheduledTaskCard.vue
@@ -1,83 +1,58 @@
 <template>
-    <Card :item="item">
-        <div class="centered-content">
-            {{ item.description }}
-        </div>
+    <GenericCard :item="item" :id="`unscheduled-task-card-${item.id}`">
+        <div class="centered-content">{{ item.description }}</div>
         <template #actions>
             <div class="centered-content flex-vertical" aria-label="actions">
                 <v-btn icon="mdi-drag" density="compact" flat draggable="true" aria-label="drag" @dragstart="onDrag"
-                    @drag="onDrag" @dragend="onDrag" ref="drag" class="cursor-drag"></v-btn>
+                    @drag="onDrag" @dragend="onDrag" :id="`unscheduled-task-card-drag-${item.id}`"
+                    class="cursor-drag"></v-btn>
                 <v-btn icon="mdi-delete-forever" density="compact" flat aria-label="delete"
                     @click="$emit('delete')"></v-btn>
                 <v-btn icon="mdi-pencil" density="compact" flat aria-label="edit" @click="$emit('edit')"></v-btn>
             </div>
         </template>
-    </Card>
+    </GenericCard>
 </template>
 
 <script lang="ts">
-import Card from './GenericCard.vue';
-import type { CreateComponentPublicInstance } from 'vue';
+import GenericCard from './GenericCard.vue';
 import DescriptiveItemType from '@/types/DescriptiveItemType';
 export default {
     props: {
-        item: {
-            type: DescriptiveItemType,
-            required: true,
-        }
+        item: { type: DescriptiveItemType, required: true, },
     },
-    data() {
-        return {
-            draggerCoords: {
-                x: 0, y: 0,
-            },
-            prevLeftOffset: {
-                x: 0, y: 0,
-            },
-        };
-    },
-    components: { Card },
-    mounted() {
-        const dragger = (this.$refs.drag as CreateComponentPublicInstance).$el as HTMLElement;
-        this.draggerCoords = { x: dragger.getBoundingClientRect().x, y: dragger.getBoundingClientRect().y };
-    },
+    components: { GenericCard, },
     methods: {
         onDrag(event: DragEvent) {
+            const card = document.getElementById(
+                `unscheduled-task-card-${this.item.id}`) as HTMLElement;
             switch (event.type) {
                 case 'dragstart':
-                    event.dataTransfer?.setDragImage(document.createElement('div'), 0, 0);
-                    break;
-                case 'drag':
-                    this._moveCard(event.clientX, event.clientY);
+                    if (!event.dataTransfer) {
+                        throw new Error("no data transfer in drag event");
+                    }
+                    event.dataTransfer.effectAllowed = 'move';
+                    event.dataTransfer.dropEffect = 'move';
+                    const drag = document.getElementById(
+                        `unscheduled-task-card-drag-${this.item.id}`) as HTMLElement;
+                    event.dataTransfer.setDragImage(
+                        card,
+                        drag.offsetLeft + drag.scrollHeight / 2,
+                        drag.offsetTop + drag.scrollWidth / 2,
+                    );
+                    card.style.opacity = '0.6';
+                    event.dataTransfer.setData('unscheduledTask', JSON.stringify(this.item));
                     break;
                 case 'dragend':
-                    this._resetCardDrag();
+                    card.style.opacity = '1.0';
+                    break;
             }
         },
-        _resetCardDrag() {
-            const card = this.$el as HTMLElement;
-            this.prevLeftOffset = { x: 0, y: 0, };
-            card.style.transform = `translate(0px, 0px)`;
-        },
-        _moveCard(draggerX: number, draggerY: number) {
-            const manHDist = Math.abs(draggerX - this.draggerCoords.x) + Math.abs(draggerY - this.draggerCoords.y);
-            if (manHDist > 50) {
-                const [offsetX, offsetY] = [draggerX - this.draggerCoords.x, draggerY - this.draggerCoords.y];
-
-                const card = this.$el as HTMLElement;
-                card.style.transform = `translate(${offsetX}px, ${offsetY}px)`;
-                card.style.zIndex = '1';
-                this.prevLeftOffset = {
-                    x: draggerX,
-                    y: draggerY,
-                };
-            }
-        }
     },
 }
 </script>
 
-<style scope>
+<style scoped>
 [aria-label='actions'] {
     gap: 1rem;
 }
