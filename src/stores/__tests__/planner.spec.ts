4d17dba7f29899784416e842d4942551bd5b06ed
Thabo Ranamane <thabo@synthesis.co.za>
2023-03-20 21:39:17 +0200

    updateTimeslot in planner store

--- src/stores/__tests__/planner.spec.ts
+++ src/stores/__tests__/planner.spec.ts
@@ -1,16 +1,101 @@
 import { TimeInMillis } from "@/middleware/helpers";
 import DescriptiveItemType from "@/types/DescriptiveItemType";
-import { beforeEach, describe, expect, it } from "vitest";
+import { beforeEach, describe, expect, it, test } from "vitest";
 import { usePlannerStore } from "../planner";
 import { faker } from '@faker-js/faker';
 import { setActivePinia, createPinia } from 'pinia'
 import TimedItemTypeWithTasks from "@/types/TimedItemTypeWithTasks";
+import ScheduledDescriptiveItemType from "@/types/ScheduledDescriptiveItemType";
 
 describe('Planner', () => {
     let planner: ReturnType<typeof usePlannerStore>;
     beforeEach(() => {
         setActivePinia(createPinia());
         planner = usePlannerStore();
+    });
+    describe('updateTimeslot', () => {
+        const plannerHasOneTimeslot = () => planner.getTimeslots().length === 1;
+        let existingTslot: TimedItemTypeWithTasks;
+        beforeEach(() => {
+            planner.clearAll();
+            const partialTslot = new TimedItemTypeWithTasks(
+                '', TimeInMillis.Hour, 10, 10, TimeInMillis.Day, [],
+            );
+            const tslotCreated = planner.createTimeslot(partialTslot);
+            expect(tslotCreated).toBeTruthy();
+
+            expect(plannerHasOneTimeslot()).toBeTruthy();
+            [existingTslot] = planner.getTimeslots();
+        });
+        test('given a timeslot with a new length and new start time such that the ' +
+            'timeslot does not intersect with any other existing timeslot and ' +
+            'effort capacity which allows its scheduled tasks to fit then ' +
+            'the timeslot should be correctly updated and true should be returned', () => {
+                const updatedTimeslot = new TimedItemTypeWithTasks(
+                    existingTslot.id, 2 * TimeInMillis.Hour, 20, 30, TimeInMillis.Day, [],
+                );
+
+                const updated = planner.updateTimeslot(updatedTimeslot);
+                expect(updated).toBeTruthy();
+                expect(plannerHasOneTimeslot()).toBeTruthy();
+                expect(planner.getTimeslots()).toContain(updatedTimeslot);
+            },
+        );
+        test('given a timeslot with a new length such that the timeslot intersects ' +
+            'with an existing timeslot then it should return false without updating ' +
+            'the timeslot', () => {
+                const lengthOfNewTslot = 10 * TimeInMillis.Minute;
+                const startTimeOfNewTslot = existingTslot.startTime - 2 * lengthOfNewTslot;
+                const anotherTimeslotBeforeExistingTimeslot = new TimedItemTypeWithTasks(
+                    '', lengthOfNewTslot, 10, 10, startTimeOfNewTslot, [],
+                );
+                const created = planner.createTimeslot(anotherTimeslotBeforeExistingTimeslot);
+                expect(created).toBeTruthy();
+
+                const newIntersectingLength = 3 * lengthOfNewTslot;
+                const updatedTslot = new TimedItemTypeWithTasks(
+                    anotherTimeslotBeforeExistingTimeslot.id,
+                    newIntersectingLength,
+                    10,
+                    10,
+                    startTimeOfNewTslot,
+                    [],
+                );
+                const updated = planner.updateTimeslot(updatedTslot);
+
+                expect(updated).toBeFalsy();
+            },
+        );
+        test('given a timeslot with a mES effort less than that required by all ' +
+            'scheduled tasks then it should return false without updating the timeslot', () => {
+                const tasksWithOverflowMES = [
+                    new ScheduledDescriptiveItemType('', 10 * TimeInMillis.Minute, 0, 10, 'desc', 0),
+                    new ScheduledDescriptiveItemType('', 10 * TimeInMillis.Minute, 0, 10, 'desc', 2_000),
+                ];
+                const mESOverCapacitedTslot = new TimedItemTypeWithTasks(
+                    '', TimeInMillis.Hour, 10, 10, 0, tasksWithOverflowMES,
+                );
+
+                const updated = planner.updateTimeslot(mESOverCapacitedTslot);
+
+                expect(updated).toBeFalsy();
+            },
+        );
+        test('given a timeslot with a pES effort less than that required by all ' +
+            'scheduled tasks then it should return false without updating the timeslot', () => {
+                const tasksWithOverflowPES = [
+                    new ScheduledDescriptiveItemType('', 10 * TimeInMillis.Minute, 20, 10, 'desc', 0),
+                    new ScheduledDescriptiveItemType('', 10 * TimeInMillis.Minute, 20, 10, 'desc', 2_000),
+                ];
+                const pESOverCapacitedTslot = new TimedItemTypeWithTasks(
+                    '', TimeInMillis.Hour, 10, 10, 0, tasksWithOverflowPES,
+                );
+
+                const updated = planner.updateTimeslot(pESOverCapacitedTslot);
+
+                expect(updated).toBeFalsy();
+            },
+        );
     });
     describe('updateTask', () => {
         it('updates the existing task', () => {
@@ -63,8 +148,8 @@ describe('Planner', () => {
                 const tslotCreated = planner.createTimeslot(partialTslot);
                 expect(tslotCreated).toBeTruthy();
 
-                const plannerOneNoTimeslot = () => planner.getTimeslots().length === 1;
-                expect(plannerOneNoTimeslot()).toBeTruthy();
+                const plannerHasOneTimeslot = () => planner.getTimeslots().length === 1;
+                expect(plannerHasOneTimeslot()).toBeTruthy();
             },
         );
         it('given a timeslot which intersects with any existing timeslot then it should just ' +
--- src/stores/planner.ts
+++ src/stores/planner.ts
@@ -2,6 +2,8 @@ import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 import type DescriptiveItemType from '@/types/DescriptiveItemType';
 import type TimedItemTypeWithTasks from '@/types/TimedItemTypeWithTasks';
+import type TimedItemType from '@/types/TimedItemType';
+import ItemType from '@/types/ItemType';
 
 export const usePlannerStore = defineStore('planner', () => {
     const id = ref<number>(0);
@@ -25,27 +27,83 @@ export const usePlannerStore = defineStore('planner', () => {
         }
     }
 
+    function clearAll() {
+        tasks.value = [];
+        timeslots.value = [];
+    }
+
+    function updateTimeslot(timeslot: TimedItemTypeWithTasks): boolean {
+        if (intersectsWithOtherTimeslots(timeslot)) {
+            return false;
+        }
+
+        if (isInOverflow(timeslot)) {
+            return false;
+        }
+
+        const index = timeslots.value.findIndex(tslot => tslot.id === timeslot.id);
+        if (index > -1) {
+            timeslots.value.splice(index, 1, timeslot);
+            return true;
+        }
+        return false;
+    }
+
+    function isInOverflow(timeslot: TimedItemTypeWithTasks): boolean {
+        const effortSum = timeslot.scheduledTasks.reduce((acc: ItemType, task: ItemType) => {
+            acc.length += task.length;
+            acc.mES += task.mES;
+            acc.pES += task.pES;
+            return acc;
+        }, new ItemType(0, 0, 0, ''));
+
+        return (
+            effortSum.length > timeslot.length ||
+            effortSum.pES > timeslot.pES ||
+            effortSum.mES > timeslot.mES
+        );
+    }
+
     function createTimeslot(idLessTimeslot: TimedItemTypeWithTasks): boolean {
         const newTimeslot = { ...idLessTimeslot, id: id.value.toString() };
-        for (let i = 0; i < timeslots.value.length; i++) {
-            const otherTimeslot = timeslots.value[i];
-            if (
-                (otherTimeslot.startTime >= newTimeslot.startTime &&
-                    otherTimeslot.startTime < newTimeslot.startTime + newTimeslot.length) ||
-                (newTimeslot.startTime >= otherTimeslot.startTime &&
-                    newTimeslot.startTime < otherTimeslot.startTime + otherTimeslot.length)
-            ) {
-                return false;
-            }
+        if (intersectsWithOtherTimeslots(newTimeslot)) {
+            return false;
         }
         timeslots.value.push(newTimeslot);
         id.value++;
         return true;
     }
 
+    function intersectsWithOtherTimeslots(newTimeslot: TimedItemType): boolean {
+        for (let i = 0; i < timeslots.value.length; i++) {
+            const otherTimeslot = timeslots.value[i];
+            if (newTimeslot.id === otherTimeslot.id) {
+                continue;
+            }
+
+            if (
+                (otherTimeslot.startTime >= newTimeslot.startTime &&
+                    otherTimeslot.startTime < newTimeslot.startTime + newTimeslot.length) ||
+                (newTimeslot.startTime >= otherTimeslot.startTime &&
+                    newTimeslot.startTime < otherTimeslot.startTime + otherTimeslot.length)
+            ) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     function getTimeslots(): Array<TimedItemTypeWithTasks> {
         return timeslots.value;
     }
 
-    return { getTasks, createTask, createTimeslot, getTimeslots, updateTask };
+    return {
+        getTasks,
+        createTask,
+        createTimeslot,
+        getTimeslots,
+        updateTask,
+        updateTimeslot,
+        clearAll,
+    };
 });
