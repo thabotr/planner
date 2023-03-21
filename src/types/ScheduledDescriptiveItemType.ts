ddeb45ebba2cd90a01499a30c3f7d38a56b94799
Thabo Ranamane <thabo@synthesis.co.za>
2023-03-21 11:09:36 +0200

    marking task as done

--- src/types/ScheduledDescriptiveItemType.ts
+++ src/types/ScheduledDescriptiveItemType.ts
@@ -1,6 +1,7 @@
 import DescriptiveItemType from "./DescriptiveItemType";
 
 class ScheduledDescriptiveItemType extends DescriptiveItemType {
+    done: boolean = false;
     startsAtMillis: number;
     constructor(id: string, length: number, pES: number, mES: number, description: string, startsAtMillis: number) {
         super(id, length, pES, mES, description);
